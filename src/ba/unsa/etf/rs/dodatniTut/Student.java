51098f44dca6df2f695249aec6c5afbdd1781294
tsijercic1 <tsijercic1@etf.unsa.ba>
2019-04-22 09:43:53 +0200

    JavaFx Bean Student

--- src/ba/unsa/etf/rs/dodatniTut/Student.java
+++ src/ba/unsa/etf/rs/dodatniTut/Student.java
@@ -1,58 +1,78 @@
 package ba.unsa.etf.rs.dodatniTut;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class Student {
-    private String ime,prezime;
-    private Integer index;
-    private LocalDate datumRođenja;
+    private SimpleStringProperty ime=new SimpleStringProperty(),prezime=new SimpleStringProperty();
+    private SimpleIntegerProperty index=new SimpleIntegerProperty();
+    private SimpleObjectProperty<LocalDate> datumRodjenja =new SimpleObjectProperty<>();
 
     public Student() {
     }
 
-    public Student(String ime, String prezime,Integer index,LocalDate datumRođenja) {
-        this.ime=ime;
-        this.prezime=prezime;
-        this.index=index;
-        this.datumRođenja = datumRođenja;
+    public Student(String ime, String prezime,Integer index,LocalDate datumRodjenja) {
+        this.ime.set(ime);
+        this.prezime.set(prezime);
+        this.index.set(index);
+        this.datumRodjenja.set(datumRodjenja);
     }
 
     public String getIme() {
+        return ime.get();
+    }
+
+    public SimpleStringProperty imeProperty() {
         return ime;
     }
 
     public void setIme(String ime) {
-        this.ime = ime;
+        this.ime.set(ime);
     }
 
     public String getPrezime() {
+        return prezime.get();
+    }
+
+    public SimpleStringProperty prezimeProperty() {
         return prezime;
     }
 
     public void setPrezime(String prezime) {
-        this.prezime = prezime;
+        this.prezime.set(prezime);
     }
 
-    public Integer getIndex() {
+    public int getIndex() {
+        return index.get();
+    }
+
+    public SimpleIntegerProperty indexProperty() {
         return index;
     }
 
-    public void setIndex(Integer index) {
-        this.index = index;
+    public void setIndex(int index) {
+        this.index.set(index);
     }
 
-    public LocalDate getDatumRođenja() {
-        return datumRođenja;
+    public LocalDate getDatumRodjenja() {
+        return datumRodjenja.get();
     }
 
-    public void setDatumRođenja(LocalDate datumRođenja) {
-        this.datumRođenja = datumRođenja;
+    public SimpleObjectProperty<LocalDate> datumRodjenjaProperty() {
+        return datumRodjenja;
+    }
+
+    public void setDatumRodjenja(LocalDate datumRodjenja) {
+        this.datumRodjenja.set(datumRodjenja);
     }
 
     @Override
     public String toString() {
         return getIme() + " " + getPrezime() + " (" + getIndex() + ")" + " - " +
-                getDatumRođenja().format(DateTimeFormatter.ofPattern("dd. M. yyyy"));
+                getDatumRodjenja().format(DateTimeFormatter.ofPattern("dd. M. yyyy"));
     }
 }
